cb61eec93fcee60e712d84f38d6d2cd255e83226
Suraj <surajgupta.college@gmail.com>
2023-02-22 18:48:38 +0530

    removed analytic

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
+// import { Analytics } from "@vercel/analytics/react";
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
-import { Analytics } from "@vercel/analytics/react";
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
@@ -10,12 +10,12 @@ export default function App({ Component, pageProps }) {
         <header className='border-b sticky top-0 bg-white z-50'>
           <Navbar />
         </header>
-        <Analytics />
         <Component {...pageProps} />
         <footer className='border-t py-9 bg-white mt-auto'>
           <Footer />
         </footer>
       </div>
+      {/* <Analytics /> */}
     </>
   )
 }
