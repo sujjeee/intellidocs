a2c9a560557f2dcc19e1abebd3edb46ceceeec43
Suraj <surajgupta.college@gmail.com>
2023-02-25 16:57:34 +0530

    added date and fixed code line

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import parse, { Element } from 'html-react-parser';
 import Image from 'next/image';
+import moment from 'moment/moment';
 
 
 const BlogPost = ({ posts }) => {
     return (
-
         <article className='w-full mb-3 '>
             <div className="w-full">
                 <div className='p-4 sm:py-8 lg:px-0 '>
                     <header>
                         <h1 className='text-2xl sm:text-3xl md:text-4xl md:tracking-wide text-black font-bold'>{posts.title}</h1>
                         <div className='pt-5 pb-8'>
-                            <time className="text-xs text-gray-500 font-medium tracking-wide">
-                                Posted on january 23, 2022
+                            <time className="text-xs text-gray-500 tracking-[0.028em]">
+                                Posted on {moment(posts.updatedAt).format("LL")}
                             </time>
                         </div>
                     </header>
-                    <div className="mb-3 text-base text-gray-700 w-full sm:text-lg prose max-w-none prose-a:text-blue-600 hover:prose-a:text-blue-500 prose-img:rounded-md prose-img:max-h-[400px] prose-img:shadow-md prose-h3:text-lg prose-h2:text-xl prose-h2:text-black prose-h3:text-black prose-h3:font-medium prose-h2:font-semibold ">
+                    <div className="mb-3 text-base text-gray-700 w-full sm:text-lg prose max-w-none prose-a:text-blue-600 hover:prose-a:text-blue-500 prose-img:rounded-md prose-img:max-h-[400px] prose-img:shadow-md prose-img:m-0 prose-h3:text-lg prose-h2:text-xl prose-h2:text-black prose-h3:text-black prose-h3:font-medium prose-h2:font-semibold ">
                         {parse(posts.content?.html, {
                             replace: (domNode) => {
                                 if (domNode instanceof Element && domNode.name === "img") {
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,54 +3,50 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>
-            <footer aria-label="Site Footer" className="bg-white">
-                <div className="max-w-screen-xl px-4  mx-auto ">
-
-                    <div className=" sm:flex sm:items-center sm:justify-between ">
-                        <nav aria-label="Footer Navigation - Support">
-                            <ul className="flex flex-wrap justify-center gap-5 lg:justify-end font-semibold tracking-wide text-xs">
-                                <li>
-                                    <Link href={`/blogs`} className="text-gray-500 transition hover:opacity-75">
-                                        Blogs
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link href={`/info/about`} className="text-gray-500 transition hover:opacity-75">
-                                        About
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link href={`/info/contact`} className="text-gray-500 transition hover:opacity-75">
-                                        Contact us
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link href="/info/privacy-policy" className="text-gray-500 transition hover:opacity-75">
-                                        Privacy Policy
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link href='/info/terms-and-conditions' className="text-gray-500 transition hover:opacity-75">
-                                        Terms and Conditions
-                                    </Link>
-                                </li>
-                            </ul>
-                        </nav>
-
-                        <ul className="flex justify-center gap-6 mt-8 sm:mt-0 lg:justify-end">
+        <footer aria-label="Site Footer" className="bg-white">
+            <div className="max-w-screen-xl px-4  mx-auto ">
+                <div className=" sm:flex sm:items-center sm:justify-between ">
+                    <nav aria-label="Footer Navigation - Support">
+                        <ul className="flex flex-wrap justify-center gap-5 lg:justify-end font-semibold tracking-wide text-xs">
                             <li>
-                                <Link href={`/`} className="text-gray-500 text-sm tracking-wide hover:opacity-75 font-semibold">
-                                    © 2023. IntelliDocs. All rights reserved.
+                                <Link href={`/blogs`} className="text-gray-500 transition hover:opacity-75">
+                                    Blogs
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={`/info/about`} className="text-gray-500 transition hover:opacity-75">
+                                    About
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={`/info/contact`} className="text-gray-500 transition hover:opacity-75">
+                                    Contact us
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/info/privacy-policy" className="text-gray-500 transition hover:opacity-75">
+                                    Privacy Policy
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href='/info/terms-and-conditions' className="text-gray-500 transition hover:opacity-75">
+                                    Terms and Conditions
                                 </Link>
                             </li>
                         </ul>
+                    </nav>
 
-                    </div>
+                    <ul className="flex justify-center gap-6 mt-8 sm:mt-0 lg:justify-end">
+                        <li>
+                            <Link href={`/`} className="text-gray-500 text-sm tracking-wide hover:opacity-75 font-semibold">
+                                © 2023. IntelliDocs. All rights reserved.
+                            </Link>
+                        </li>
+                    </ul>
+
                 </div>
-            </footer>
-
-        </div>
+            </div>
+        </footer>
     )
 }
 
